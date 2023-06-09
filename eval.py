8a9c0204a02be7682a8aca7b156a5c367770467d
UntotaufUrlaub <sturmjakob@yahoo.de>
2023-06-09 15:57:51 +0200

    eval.py: make device configurable

--- eval.py
+++ eval.py
@@ -4,8 +4,15 @@ import ast
 
 source = os.getenv('DOCUMENT')
 hypothesis = os.getenv('SUMMARY')
+device = os.getenv('Device')
+if device == 'cpu':
+    isCude = False
+elif device == 'gpu':
+    isCuda = True
+else:
+    raise ValueError(f"Unknown devise: {device}")
 
-questeval = QuestEval()
+questeval = QuestEval( isCuda=isCuda)
 
 score = questeval.corpus_questeval(hypothesis=[hypothesis], sources=[source])
 print('Result is: ' + str(score['ex_level_scores'][0]))
