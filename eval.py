1a02478d8c796c8ec3ea4290465216b744c7c2bc
UntotaufUrlaub <sturmjakob@yahoo.de>
2023-06-09 15:53:56 +0200

    eval.py: device configurable

--- eval.py
+++ eval.py
@@ -4,8 +4,15 @@ import os
 source = os.getenv('DOCUMENT')
 hypothesis = os.getenv('SUMMARY')
 metric_type = os.getenv('TYPE')
+device = os.getenv('Device')
+if device == 'cpu':
+    isCude = False
+elif device == 'gpu':
+    isCuda = True
+else:
+    raise ValueError(f"Unknown devise: {device}")
 
-questeval = QuestEval(task="summarization", do_weighter=True)
+questeval = QuestEval(task="summarization", do_weighter=True, isCuda=isCuda)
 
 score = questeval.compute_all(hypothesis, source)
 if metric_type is None:
