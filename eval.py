1681b389ef015246b4529667084106acc7c462dc
UntotaufUrlaub <sturmjakob@yahoo.de>
2023-06-10 11:28:30 +0200

    eval.py: make batch size configurable

--- eval.py
+++ eval.py
@@ -11,8 +11,15 @@ elif device == 'gpu':
     isCuda = True
 else:
     raise ValueError(f"Unknown devise: {device}")
+qg_batch_size = os.getenv("QG_BATCH_SIZE")
+qg_batch_size = int(qg_batch_size) if qg_batch_size is not None else None
+clf_batch_size = os.getenv("CLF_BATCH_SIZE")
+clf_batch_size = int(clf_batch_size) if clf_batch_size is not None else None
 
-questeval = QuestEval(task="summarization", do_weighter=True, isCuda=isCuda)
+questeval = QuestEval(task="summarization", do_weighter=True, isCuda=isCuda,
+                      qg_batch_size=qg_batch_size,
+                      clf_batch_size=clf_batch_size
+                      )
 
 score = questeval.compute_all(hypothesis, source)
 if metric_type is None:
